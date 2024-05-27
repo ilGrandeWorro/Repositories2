42736d178060b40350e4337cc003964b17bd8c7e
Worro <alessiodelledonne29@gmail.com>
2024-05-27 11:04:53 +0200

    made some corrections

--- src/main/java/com/worro/repositories2/programmingLanguages/ProgrammingLanguages.java
+++ src/main/java/com/worro/repositories2/programmingLanguages/ProgrammingLanguages.java
@@ -1,19 +1,13 @@
 package com.worro.repositories2.programmingLanguages;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
-@Table
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ProgrammingLanguages {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
@@ -23,4 +17,44 @@ public class ProgrammingLanguages {
 
     @Column(nullable = false)
     private String inventor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
+
+    public int getFirstAppereance() {
+        return firstAppereance;
+    }
+
+    public void setFirstAppereance(int firstAppereance) {
+        this.firstAppereance = firstAppereance;
+    }
+
+    public String getInventor() {
+        return inventor;
+    }
+
+    public void setInventor(String inventor) {
+        this.inventor = inventor;
+    }
+
+    public ProgrammingLanguages(long id, String languageName, int firstAppereance, String inventor) {
+        this.id = id;
+        this.languageName = languageName;
+        this.firstAppereance = firstAppereance;
+        this.inventor = inventor;
+    }
+    public ProgrammingLanguages(){}
 }
